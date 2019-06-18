df62b676af98f8974ab47236c43048006f147a6a
Mmirzax <hahdi.mirza@gmail.com>
2019-06-18 17:36:37 -0400

    removal jquery

--- Tuts/js/script.js
+++ Tuts/js/script.js
@@ -36,7 +36,7 @@
 
 //     $("#list").parent().css("background-color", "rgba(180,180,180,0.4");
 
-     
+
 //     $("#list").siblings("p").css("background-color", "rgba(180,180,180,0.8");
 //     $("#list").siblings(":header").css("background-color", "rgba(180,180,180,0.8");
 
@@ -64,32 +64,79 @@
 // })
 
 
-$(function () {
-    //  $("ul ul:first").append("<li> i'm going to be the last sub-item!");
-    // $("<li> i'm going to be the last sub-item!</li>").appendTo("ul ul:first");
+// $(function () {
+//     $("ul ul:first").append("<li> i'm going to be the last sub-item!");
+//     $("<li> i'm going to be the last sub-item!</li>").appendTo("ul ul:first");
 
-    // $("ul ul:first").prepend("<li> i'm going to be the first sub-item!</li>");
-    // $("ul ul").prepend("<li> i'm going to be the first sub-item!</li>");
-    // $("<li> i'm going to be the first sub-item!</li>").prependTo("ul ul:first");
-    $("<h4> Muhammad Mirza </h4>").prependTo("#content");
+//     $("ul ul:first").prepend("<li> i'm going to be the first sub-item!</li>");
+//     $("ul ul").prepend("<li> i'm going to be the first sub-item!</li>");
+//     $("<li> i'm going to be the first sub-item!</li>").prependTo("ul ul:first");
+//     $("<h4> Muhammad Mirza </h4>").prependTo("#content");
 
-   // $(".red-box").after("<div class='red-box'>After Red Box </div>");
-   // $(".blue-box").before("<div class='blue-box'>Before blue Box </div>")
+//    $(".red-box").after("<div class='red-box'>After Red Box </div>");
+//    $(".blue-box").before("<div class='blue-box'>Before blue Box </div>")
 //    $(".blue-box").before(function() {
 //        return "<div class='blue-box'>Blue 2</div>"
 //    });
-    // $(".blue-box").before($(".red-box"));
-    // $("p :first").after($("#list"));
+//     $(".blue-box").before($(".red-box"));
+//     $("p :first").after($("#list"));
 
-    $(function() {
-        var el = $('h1').siblings('div');
-        console.log(el);
-        el.each(function (index) {
-          if (index < 3) {
-            $(this).after($(this).clone());
-          }
-        });
-      });
-    
+//     $(function() {
+//         var el = $('h1').siblings('div');
+//         console.log(el);
+//         el.each(function (index) {
+//           if (index < 3) {
+//             $(this).after($(this).clone());
+//           }
+//         });
+//       });
+
+
+// });
+
+
+$(function () {
+    // $("li :last").replaceWith("<li>Replaced.</li>")
+
+    // $("li :first").replaceWith(function(){
+    //     return "<li>replaced with function</li>";
+
+    // })
+    // var firstListItem = $("ul :first")
+    // $("p").replaceWith(firstListItem);
+
+    // var greenbox = $(".green-box");
+
+    // var el = $('h1').siblings('div');
+    // console.log(el);
+    // el.each(function (index) {
+    //     if (index < 3) {
+    //         $(this).replaceWith(greenbox.clone());
+    //     }
+    // });
+
+    // $("<div class = 'green-box'>More Green!</div>").replaceAll(".red-box,.blue-box")
+
+
+});
+
+
+$(function () {
+    // $("li").remove();
+    $('form').children().not("input:text,textarea,br").remove();
+
+
+    var detachedListItem = $("li").detach();
+
+    $("#content").append(detachedListItem);
+
+    $("p:first").empty();
+
+
+    $(".red-box, .green-box, .blue-box").empty();
+
+
+
+
 
 });
