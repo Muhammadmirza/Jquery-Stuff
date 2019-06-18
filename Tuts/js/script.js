7f5f19b7cf452c9f2af4ff09946dfe068c435542
Mmirzax <hahdi.mirza@gmail.com>
2019-06-18 09:35:49 -0400

    filter function, traversal, not, jquery.

--- Tuts/js/script.js
+++ Tuts/js/script.js
@@ -1,51 +1,70 @@
-$(function () {
-    // jQuery goes here...
+// $(function () {
+//     jQuery goes here...
 
-    // Uncomment this line to fade out the red box on page load
-    // $(".red-box").fadeOut(2000);
-    // $(".green-box").fadeOut(2000);
-    // $(".red-box").fadeIn(1000);
-    // $(".green-box").fadeOut(1000);
-    // $(".green-box").fadeTo(1000,0.5);
-    // $(".red-box").fadeTo(1000, 0);
-    // $(".green-box").fadeTo(1000, 0.5);
-    // $(".blue-box").fadeTo(1000, 0.8);
-    // $(".green-box").fadeToggle();
-    // $(".green-box").fadeToggle();
-    // $(".blue-box").hide(1000);
-    // $(".blue-box").show(1000);
-    // $(".blue-box").slideToggle(2000);
-    // $('p').hide();
-    // $('p').slideDown(1500);
+//     Uncomment this line to fade out the red box on page load
+//     $(".red-box").fadeOut(2000);
+//     $(".green-box").fadeOut(2000);
+//     $(".red-box").fadeIn(1000);
+//     $(".green-box").fadeOut(1000);
+//     $(".green-box").fadeTo(1000,0.5);
+//     $(".red-box").fadeTo(1000, 0);
+//     $(".green-box").fadeTo(1000, 0.5);
+//     $(".blue-box").fadeTo(1000, 0.8);
+//     $(".green-box").fadeToggle();
+//     $(".green-box").fadeToggle();
+//     $(".blue-box").hide(1000);
+//     $(".blue-box").show(1000);
+//     $(".blue-box").slideToggle(2000);
+//     $('p').hide();
+//     $('p').slideDown(1500);
 
-    // $('.blue-box').animate({
-    //     "margin-left":"+=200px",
-    // }, 1000, "linear")
-    // $('.blue-box').animate({
-    //     "margin-left":"-=200px",
-    // }, 1000, "linear")
+//     $('.blue-box').animate({
+//         "margin-left":"+=200px",
+//     }, 1000, "linear")
+//     $('.blue-box').animate({
+//         "margin-left":"-=200px",
+//     }, 1000, "linear")
+
+// });
+
+// $(function () {
+//     $("#list").find("li").css("background-color", "rgba(180,180,180,0.4");
+
+//     $("#list").children("li").css("background-color", "rgba(180,180,180,0.4");
+
+//     $("#list").parents("body").css("background-color", "rgba(180,180,180,0.4");
+
+//     $("#list").parent().css("background-color", "rgba(180,180,180,0.4");
+
+//     $("#list").siblings().css("background-color", "rgba(180,180,180,0.8");
+//     $("#list").siblings("p").css("background-color", "rgba(180,180,180,0.8");
+//     $("#list").siblings(":header").css("background-color", "rgba(180,180,180,0.8");
+
+//     $("#list").prev().css("background-color", "rgba(180,180,180,0.8");
+//     $("#list").next().css("background-color", "rgba(180,180,180,0.8");
+//     $("#list").prev().next().css("background-color", "rgba(180,180,180,0.8");
+//     $(':header').next().css("background-color", "purple");
+//     $('input:password').css("background-color", "purple");
+// })
+
+// $(function () {
+// //    $("li").filter(function(index){
+// //        return index % 3===1;
+// //    }).css("background-color", "rgba(180,180,180,0.8)"); 
+// //    $('li').eq(3).css("background-color", 'rgba(180,180,180,0.8)');  
+// //    $('li').eq(-2).css("background-color", 'rgba(180,180,180,0.8)');  
+// //    $('li').first().css("background-color", 'rgba(180,180,180,0.8)');
+// //    $('li').last().css("background-color", 'rgba(180,180,180,0.8)');
+// //    $('li').not(":first").css("background-color", 'rgba(180,180,180,0.8)');
+// //    $('li').not("#list ul li").css("background-color", 'rgba(180,180,180,0.8)');
+// //     $('li').not(function(index){
+// //         return index % 3 ===2;
+// //     }).css("background-color", 'rgba(180,180,180,0.3)');
+
+// })
+
+
+$(function(){
+    $("ul ul:first").append("<li> i'm going to be the last sub-item!");
 
 });
-
-$(function () {
-    // $("#list").find("li").css("background-color", "rgba(180,180,180,0.4");
-
-    // $("#list").children("li").css("background-color", "rgba(180,180,180,0.4");
-
-    // $("#list").parents("body").css("background-color", "rgba(180,180,180,0.4");
-
-    // $("#list").parent().css("background-color", "rgba(180,180,180,0.4");
-
-    // $("#list").siblings().css("background-color", "rgba(180,180,180,0.8");
-    // $("#list").siblings("p").css("background-color", "rgba(180,180,180,0.8");
-    // $("#list").siblings(":header").css("background-color", "rgba(180,180,180,0.8");
-
-    // $("#list").prev().css("background-color", "rgba(180,180,180,0.8");
-    // $("#list").next().css("background-color", "rgba(180,180,180,0.8");
-    // $("#list").prev().next().css("background-color", "rgba(180,180,180,0.8");
-    $(':header').next().css("background-color", "purple")
-    $('input:password').css("background-color", "purple")
-
-
-
-})
